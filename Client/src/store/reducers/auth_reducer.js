0e3d137fc8fa0a7242fe3b3e027cb4a6800bb63c
koralle <mkhd7930@gmail.com>
2020-01-19 23:36:28 +0900

    Revert "PostsReducerコンポーネントの再定義"
    This reverts commit 7b35d45d7b76fced759379e825087d9ef3081495.

--- Client/src/store/reducers/auth_reducer.js
+++ Client/src/store/reducers/auth_reducer.js
@@ -1,44 +1,22 @@
 import * as ACTION_TYPES from '../actions/action_types';
 
 export const initialState = {
-  is_authenticated: false,
-  db_profile: null,
-  profile: null,
+  posts: null,
 };
 
-export const AuthReducer = (state = initialState, action) => {
+export const PostReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ACTION_TYPES.LOGIN_SUCCESS:
+    case ACTION_TYPES.FETCH_DB_POSTS:
       return {
         ...state,
-        is_authenticated: true
+        posts: action.payload,
       };
-    case ACTION_TYPES.LOGIN_FAILURE:
+    case ACTION_TYPES.REMOVE_DB_POSTS:
       return {
         ...state,
-        is_authenticated: false
-      };
-    case ACTION_TYPES.ADD_PROFILE:
-      return {
-        ...state,
-        profile: action.payload
-      };
-    case ACTION_TYPES.REMOVE_PROFILE:
-      return {
-        ...state,
-        profile: null
-      };
-    case ACTION_TYPES.SET_DB_PROFILE:
-      return {
-        ...state,
-        db_profile: action.payload
-      };
-    case ACTION_TYPES.REMOVE_DB_PROFILE:
-      return {
-        ...state,
-        db_profile: null
+        posts: []
       };
     default:
-      return state
-  };
+      return state;
+  }
 };
